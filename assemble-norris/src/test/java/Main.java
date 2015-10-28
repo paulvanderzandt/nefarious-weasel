94645948535382bcfd935dbd338c089f64c71bbb
Thomas Biesaart <thomas.biesaart@outlook.com>
2015-10-28 20:02:54 +0100

    Clean up

---
+++ assemble-norris/src/test/java/Main.java
@@ -0,0 +1,10 @@
+import org.jaxlan.assemble.Assemble;
+
+/**
+ * Created by Thomas Biesaart on 28/10/2015.
+ */
+public class Main {
+    public static void main(String... args ) {
+        System.out.println(Assemble.assemble());
+    }
+}
